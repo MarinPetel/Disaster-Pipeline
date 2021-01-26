0a7305426e8c6d362d051a9ee5064b0725ea9eba
Marin Petel <mrnpetel@gmail.com>
2021-01-26 10:05:51 -0500

    update the loading data step for train_classifier script

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,9 +1,35 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
+# import nlp tools
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
+    '''Load the databse and return the features and targets as dataframes
 
+    INPUT:
+    database_filepath-string, the path of the database to load_data
+
+    OUTPUT:
+    X-Dataframe, the dataframe with the original features
+    Y-Dataframe, the dataframe with the targets
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql(sql='SELECT * FROM Disaster',con=engine)
+    # need to filter out some messages which have value of 2 for the categorie
+    # 'related'
+    df = df[df['related']!=2]
+    X = df['message']
+    Y = df.drop(columns=['id','message','original','genre'],axis=1)
+    return X,Y
 
 def tokenize(text):
     pass
@@ -27,13 +53,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
